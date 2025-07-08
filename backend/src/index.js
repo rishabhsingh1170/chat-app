a5c2af4f06f6b368340e4fd0ca4d4f7ca18adbbc
rishabhsingh1170 <rishabhsingh1170@gmail.com>
2025-07-08 17:42:04 +0530

    change in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,17 @@ import cors from "cors"
 import { app , server } from "./lib/socket.js";
 
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
+//const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -29,12 +34,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
