cdccb3b1034ed832c617e44d2fd819c09e0d4611
rishabhsingh1170 <rishabhsingh1170@gmail.com>
2025-07-08 17:22:13 +0530

    _dirname to __dirname

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -30,10 +30,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname,"../frontend/dist")));
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
   app.get("*", (req,res) => {
-    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
 }
 
